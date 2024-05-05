1e1e912cdda73078102e38054278eb8c7aa93148
Adrian Orenstein <adrian.orenstein@gmail.com>
2024-05-04 19:17:39 -0600

    remove parallel

--- slurm_job.sh
+++ slurm_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-mbowling
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --time=0-0:20
+#SBATCH --time=0-0:40
 
 # setup and tear down takes .5-2 minutes.
 
@@ -14,33 +14,32 @@ fi
 
 cp /home/aorenste/projects/def-mbowling/aorenste/
 
-echo "Copying virtualenv..." && \ 
-    cp ~/projects/def-mbowling/aorenste/venv.tar.gz $SLURM_TMPDIR/ && \ 
-    cd $SLURM_TMPDIR && \ 
-    tar -xzf venv.tar.gz && \ 
-    rm venv.tar.gz &
+echo "Copying virtualenv..."
+cp ~/projects/def-mbowling/aorenste/venv.tar.gz $SLURM_TMPDIR/
+cd $SLURM_TMPDIR 
+tar -xzf venv.tar.gz
+rm venv.tar.gz
 
-echo "Setting up SOCKS5 proxy..." && \ 
-    ssh -q -N -T -f -D 8888 `echo $SSH_CONNECTION | cut -d " " -f 3` && \ 
-    export ALL_PROXY=socks5h://localhost:8888
+echo "Setting up SOCKS5 proxy..."
+ssh -q -N -T -f -D 8888 `echo $SSH_CONNECTION | cut -d " " -f 3`
+export ALL_PROXY=socks5h://localhost:8888
 
-echo "Cloning repo..." && \ 
-    git config --global http.proxy 'socks5://127.0.0.1:8888' && \ 
-    git clone --quiet https://github.com/AdrianOrenstein/async-mdp.git $SLURM_TMPDIR/project &
+echo "Cloning repo..."
+git config --global http.proxy 'socks5://127.0.0.1:8888'
+git clone --quiet https://github.com/AdrianOrenstein/async-mdp.git $SLURM_TMPDIR/project
 
 # wait for background jobs to complete
 echo "Waiting for background jobs to complete..."
-wait
 
 cd $SLURM_TMPDIR/project
 
-echo "Exporting env variables" && \ 
-    export PYTHONPATH=./src:. && \ 
-    export python_venv=$SLURM_TMPDIR/virtualenvs/async-mdp-PEBw-NfQ-py3.10/bin/python3.10
+echo "Exporting env variables"
+export PYTHONPATH=./src:.
+export python_venv=$SLURM_TMPDIR/virtualenvs/async-mdp-PEBw-NfQ-py3.10/bin/python3.10
 
-echo "Running experiment..." && \ 
-    cd $SLURM_TMPDIR/project && \ 
-    $python_venv $@ 
+echo "Running experiment..."
+cd $SLURM_TMPDIR/project
+$python_venv $@ 
     # && \ 
     # echo "Syncing wandb..." && \ 
     # $python_venv -m wandb sync $SLURM_TMPDIR/project/wandb/
